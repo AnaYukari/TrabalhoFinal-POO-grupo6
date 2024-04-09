a54859ae15d5255e9673854d15c007c846c4ecd4
Aosil803 <aosil803@gmail.com>
2024-04-09 15:40:45 -0300

    adicionando arquivos

--- trabalhoFinalPOO/src/org/javaFit/classes/PersonalTrainer.java
+++ trabalhoFinalPOO/src/org/javaFit/classes/PersonalTrainer.java
@@ -2,4 +2,48 @@ package org.javaFit.classes;
 //subclasse de Pessoa
 public class PersonalTrainer extends Pessoa {
 
+    private String especialidade;
+    private String CREF;
+    private String horarioAtendimento;
+
+        public PersonalTrainer(String especialidade, String cref, String horarioAtendimento) {
+		this.especialidade = especialidade;
+		CREF = cref;
+		this.horarioAtendimento = horarioAtendimento;
+	}
+
+		public String getEspecialidade() {
+			return especialidade;
+		}
+
+		public void setEspecialidade(String especialidade) {
+			this.especialidade = especialidade;
+		}
+
+		public String getCREF() {
+			return CREF;
+		}
+
+		public void setCREF(String cREF) {
+			CREF = cREF;
+		}
+
+		public String getHorarioAtendimento() {
+			return horarioAtendimento;
+		}
+
+		public void setHorarioAtendimento(String horarioAtendimento) {
+			this.horarioAtendimento = horarioAtendimento;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("""
+					Especialidade: %s
+					CREF: %s
+					Hora de Atendimento: %s
+					""", especialidade, CREF, horarioAtendimento);
+					
+		}
+
 }
--- trabalhoFinalPOO/src/org/javaFit/classes/Pessoa.java
+++ trabalhoFinalPOO/src/org/javaFit/classes/Pessoa.java
@@ -1,5 +1,21 @@
 package org.javaFit.classes;
+
+import java.time.LocalDate;
+
 //classe mãe de Aluno, Funcionario e PersonalTrainer
 public class Pessoa {
+		private String nome;
+	    private String cpf;
+	    private LocalDate dataNascimento;
+	    private String contato;
+	    private String senha;
+	    
+		public Pessoa(String nome, String cpf, LocalDate dataNascimento, String contato, String senha) {
+			this.nome = nome;
+			this.cpf = cpf;
+			this.dataNascimento = dataNascimento;
+			this.contato = contato;
+			this.senha = senha;
+		}
 
 }
